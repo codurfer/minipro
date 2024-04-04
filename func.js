7c83a68c993af60da7ea376c64a1703029f012e2
설병진 (Byeongjin Seol) <155459955+codurfer@users.noreply.github.com>
2024-04-04 17:25:39 +0900

    Update func.js

--- func.js
+++ func.js
@@ -13,6 +13,7 @@ startBtn.addEventListener('click', (e) => {
   setTimeout('info3()', 2700);
   setTimeout('info4()', 3300);
   setTimeout('info5()', 4700);
+  setTimeout('info6()', 5300);
 });
 
 closeBtn.addEventListener('click', (e) => {
@@ -33,7 +34,7 @@ function info1() {
   creMs.classList.add('info1');
   creMs.innerHTML = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info1').style.width = '55%'", 1000);
+  setTimeout("document.querySelector('.info1').style.width = '85%'", 1000);
   setTimeout(
     "document.querySelector('.info1').innerHTML = '안녕하세요? 이건 저를 소개하는 설명봇입니다.'",
     1000
@@ -46,9 +47,9 @@ function info2() {
   creMs.classList.add('info2');
   creMs.textContent = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info2').style.width = '60%'", 1000);
+  setTimeout("document.querySelector('.info2').style.width = '85%'", 1000);
   setTimeout(
-    "document.querySelector('.info2').textContent = '설명봇이 한 가지를 설명할 거에요!'",
+    "document.querySelector('.info2').textContent = '설명봇이 한 가지를 주제에 대해서 설명할 거에요!'",
     1000
   );
 }
@@ -59,7 +60,7 @@ function info3() {
   creMs.classList.add('info3');
   creMs.textContent = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info3').style.width = '62%'", 1000);
+  setTimeout("document.querySelector('.info3').style.width = '85%'", 1000);
   setTimeout(
     "document.querySelector('.info3').textContent = '그것은 #듀공을 아세요? 입니다.'",
     1000
@@ -72,9 +73,9 @@ function info4() {
   creMs.classList.add('info4');
   creMs.textContent = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info4').style.width = '62%'", 1000);
+  setTimeout("document.querySelector('.info4').style.width = '85%'", 1000);
   setTimeout(
-    "document.querySelector('.info4').innerHTML = '듀공을 모르신다면.. 한번 검색을 해보세요.갑자기 듀공이 왜 나왔는지 궁금하시죠? 제가 물을 정말 좋아합니다. 그래서 바다와 계곡에서 물 만난 듀공이라고 불렸죠.<br>P.S 물속에서 수영하는 제 모습은 듀공을 빼닮았답니다! '",
+    "document.querySelector('.info4').innerHTML = '갑자기 듀공이 왜 나왔는지 궁금하시죠? 제가 물을 정말 좋아해요. 그래서 바다와 계곡에서 노는 모습이 마치 물 만난 듀공의 모습이라~'",
     1000
   );
 }
@@ -85,9 +86,20 @@ function info5() {
   creMs.classList.add('info5');
   creMs.textContent = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info5').style.width = '90%'", 1000);
+  setTimeout("document.querySelector('.info5').style.width = '85%'", 1000);
   setTimeout(
     `document.querySelector('.info5').innerHTML = '<img src="https://github.com/codurfer/minipro/blob/main/du0.png?raw=true" />'`,
     1000
   );
 }
+
+function info6() {
+  const creMs = document.createElement('div');
+  creMs.classList.add('btnbot');
+  creMs.classList.add('info6');
+  creMs.textContent = '...';
+  content.appendChild(creMs);
+  setTimeout("document.querySelector('.info6').style.width = '5%'", 1000);
+  setTimeout("document.querySelector('.info6').innerHTML = '끗'", 1000);
+}
+
