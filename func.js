97e4c7606c48438223eb72b1fd3cf7c2de4c9395
설병진 (Byeongjin Seol) <155459955+codurfer@users.noreply.github.com>
2024-04-04 22:51:36 +0900

    Update func.js

--- func.js
+++ func.js
@@ -15,7 +15,10 @@ startBtn.addEventListener('click', (e) => {
   setTimeout('info4()', 3300);
   setTimeout('info5()', 4700);
   setTimeout('info6()', 5300);
-  setTimeout('stopInfo()', 5800);
+  setTimeout('info7()', 6800);
+  setTimeout('info8()', 7300);
+  setTimeout('info9()', 8700);
+  setTimeout('stopInfo()', 9000);
 });
 
 closeBtn.addEventListener('click', (e) => {
@@ -69,7 +72,7 @@ function info3() {
   content.appendChild(creMs);
   setTimeout("document.querySelector('.info3').style.width = '68%'", 1000);
   setTimeout(
-    "document.querySelector('.info3').textContent = '주제는 #듀공을 아세요? 입니다.'",
+    "document.querySelector('.info3').textContent = '그것은 #듀공을 아세요? 입니다.'",
     1000
   );
 }
@@ -80,10 +83,10 @@ function info4() {
   creMs.classList.add('info4');
   creMs.textContent = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info4').style.width = '85%'", 1000);
+  setTimeout("document.querySelector('.info4').style.width = '85%'", 1100);
   setTimeout(
-    "document.querySelector('.info4').innerHTML = '갑자기 듀공이 왜 나왔는지 궁금하시죠? 제가 물을 정말 좋아해요. 그래서 바다와 계곡에서 노는 모습이 마치 물 만난 듀공의 모습이라~'",
-    1000
+    "document.querySelector('.info4').innerHTML = '갑자기 듀공이 왜 나왔는지 궁금하시죠? 제가 물을 정말 좋아해요. 그래서 바다와 계곡에서 노는 모습이 마치 물 만난 듀공의 모습이랄까~?'",
+    1100
   );
 }
 
@@ -95,7 +98,7 @@ function info5() {
   content.appendChild(creMs);
   setTimeout("document.querySelector('.info5').style.width = '85%'", 1000);
   setTimeout(
-    `document.querySelector('.info5').innerHTML = '<img src="https://github.com/codurfer/minipro/blob/main/du0.png?raw=true" />'`,
+    "document.querySelector('.info5').innerHTML = '그 정도로 물에서 할 수 있는 활동을 많이 좋아합니다. 그러다보니 수영 4가지 영법은 기본적으로 할 수 있습니다. 그리고 물과 관련된 가장 큰 관심사는 어드밴스드 오픈워터 자격입니다.'",
     1000
   );
 }
@@ -106,7 +109,45 @@ function info6() {
   creMs.classList.add('info6');
   creMs.textContent = '...';
   content.appendChild(creMs);
-  setTimeout("document.querySelector('.info6').style.width = '5%'", 1000);
-  setTimeout("document.querySelector('.info6').innerHTML = '끗'", 1000);
+  setTimeout("document.querySelector('.info6').style.width = '85%'", 1000);
+  setTimeout(
+    "document.querySelector('.info6').innerHTML = '오픈워터 자격은 2박 3일 또는 3박 4일 일정으로 진행이 됩니다. 운전 연수처럼 오픈워터 교육 비용이 있습니다. 거기에 저는 해외여행을 좋아하고 세부에서 교육 받고 싶었습니다. 근데 생각보다 비용적인 측면을 생각하지 않을 수 없었습니다. (생각 외로 많이 비싸요!)'",
+    1000
+  );
 }
 
+function info7() {
+  const creMs = document.createElement('div');
+  creMs.classList.add('btnbot');
+  creMs.classList.add('info7');
+  creMs.textContent = '...';
+  content.appendChild(creMs);
+  setTimeout("document.querySelector('.info7').style.width = '85%'", 1000);
+  setTimeout(
+    "document.querySelector('.info7').innerHTML = '해외에서 교육 받는 것을 포기하고 빠르면 올해, 늦으면 내년에 국내 오픈워터 교육 받아보려고 계획 중에 있습니다. '",
+    1000
+  );
+}
+
+function info8() {
+  const creMs = document.createElement('div');
+  creMs.classList.add('btnbot');
+  creMs.classList.add('info8');
+  creMs.textContent = '...';
+  content.appendChild(creMs);
+  setTimeout("document.querySelector('.info8').style.width = '85%'", 1000);
+  setTimeout(
+    `document.querySelector('.info8').innerHTML = '<img src="/image/du0.png" />'`,
+    1000
+  );
+}
+
+function info9() {
+  const creMs = document.createElement('div');
+  creMs.classList.add('btnbot');
+  creMs.classList.add('info9');
+  creMs.textContent = '...';
+  content.appendChild(creMs);
+  setTimeout("document.querySelector('.info9').style.width = '5%'", 1000);
+  setTimeout("document.querySelector('.info9').innerHTML = '끗'", 1000);
+}
